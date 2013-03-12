ba47495960f6257492216e0462c01a9d989cc5a7
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-03-12 10:14:07 -0300

    git...

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,10 +10,14 @@ fi
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
+alias gpa='git push origin --all'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gb='git branch'
+alias ga='git add'
+alias gaa='git add -A'
+alias gcam='git commit -a -m'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
