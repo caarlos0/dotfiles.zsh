e21f2f20fd38a55ac6bc4d1398cf2bba77b84e71
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-06-13 23:00:59 -0300

    mvnnew

--- mvn/aliases.zsh
+++ mvn/aliases.zsh
@@ -9,10 +9,11 @@ alias mvnc='mvn clean'
 alias mvncom='mvn compile'
 alias mvnt='mvn test'
 alias mvnag='mvn archetype:generate'
+alias mvnnew='mvn archetype:generate -DarchetypeArtifactId=maven-archetype-quickstart'
 
-function listMavenCompletions { 
+function listMavenCompletions {
 	 reply=(
-		cli:execute cli:execute-phase archetype:generate generate-sources compile clean install test test-compile deploy package cobertura:cobertura jetty:run gwt:run gwt:debug -DskipTests -Dmaven.test.skip=true -DarchetypeCatalog=http://tapestry.formos.com/maven-snapshot-repository -Dtest= `if [ -d ./src ] ; then find ./src -type f | grep -v svn | sed 's?.*/\([^/]*\)\..*?-Dtest=\1?' ; fi`); 
+		cli:execute cli:execute-phase archetype:generate generate-sources compile clean install test test-compile deploy package cobertura:cobertura jetty:run gwt:run gwt:debug -DskipTests -Dmaven.test.skip=true -DarchetypeCatalog=http://tapestry.formos.com/maven-snapshot-repository -Dtest= `if [ -d ./src ] ; then find ./src -type f | grep -v svn | sed 's?.*/\([^/]*\)\..*?-Dtest=\1?' ; fi`);
 }
 
 compctl -K listMavenCompletions mvn
