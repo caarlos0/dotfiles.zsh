853c9be6e07deea0a53cd232886addde9a3d1927
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-06-29 15:56:06 -0300

    grep alias, npm aliases

---
+++ npm/aliases.zsh
@@ -0,0 +1,4 @@
+alias npis='npm install --save'
+alias npig='npm install -g'
+alias npit='npm init'
+alias npi='npm install'
--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,3 +8,6 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+
+alias grep='grep --color=auto'
+alias lsopenports='lsof -i -n -P'
