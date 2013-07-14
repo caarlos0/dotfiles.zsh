110424a1fe7da3f5c175a1a7741e9e58451093fa
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-07-14 18:32:28 -0300

    adb path

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,5 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export PATH="/Users/carlos/Downloads/adt-bundle-mac-x86_64-20130522/sdk/platform-tools:$PATH"
