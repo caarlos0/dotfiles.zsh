e70ebc5ce3e0abd482aa163adeb85a3b23b17a3d
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-07-14 18:32:46 -0300

    current path is more useful than hostname in title

--- zsh/window.zsh
+++ zsh/window.zsh
@@ -12,7 +12,7 @@ function title() {
     print -Pn "\ek$a:$3\e\\" # screen title (in ^A")
     ;;
   xterm*|rxvt)
-    print -Pn "\e]2;$2\a" # plain xterm title ($3 for pwd)
+    print -Pn "\e]2;$3\a" # plain xterm title ($3 for pwd)
     ;;
   esac
 }
