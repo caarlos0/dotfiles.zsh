b7f4a2e79a4096555c5aeb1689c37e3b707a8089
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-07-14 19:52:26 -0300

    postgres.app path

--- system/path.zsh
+++ system/path.zsh
@@ -3,3 +3,5 @@ export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
 export PATH="/Users/carlos/Downloads/adt-bundle-mac-x86_64-20130522/sdk/platform-tools:$PATH"
+
+export PATH="/Applications/Postgres.app/Contents/MacOS/bin:$PATH"
