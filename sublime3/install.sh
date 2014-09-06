b0774bd04329d98abaf7206e633001b196bf6fc1
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-09-06 16:25:37 -0300

    removed sublime-text-3

--- sublime3/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-#ln -s $ZSH/sublime3/User "${HOME}/Library/Application Support/Sublime Text 3/Packages/"
-ln -s "${ZSH}/sublime3/Installed Packages" "${HOME}/Library/Application Support/Sublime Text 3/"
-ln -s "${ZSH}/sublime3/Packages" "${HOME}/Library/Application Support/Sublime Text 3/"
