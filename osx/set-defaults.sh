c4551b1bb4808fd8534fbf21f0be88968b57cafa
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-09-07 20:24:12 -0300

    updated osx/set-defaults

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -5,12 +5,12 @@
 # The original idea (and a couple settings) were grabbed from:
 #   https://github.com/mathiasbynens/dotfiles/blob/master/.osx
 # More from:
-#   https://gist.github.com/brandonb927/3195465
+#    https://gist.github.com/brandonb927/3195465
 #
 # Run ./set-defaults.sh and you'll be good to go.
 if [ "$(uname -s)" != "Darwin" ]
 then
-	exit 0
+  exit 0
 fi
 
 # Disable press-and-hold for keys in favor of key repeat.
