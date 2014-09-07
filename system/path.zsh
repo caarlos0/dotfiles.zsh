ce63d55018548d2b70d3bdee0f3ed5dcc840b26d
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-09-07 20:26:33 -0300

    better system path

--- system/path.zsh
+++ system/path.zsh
@@ -1,13 +1,10 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
-
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
-if [ -e /Applications/Postgres.app ]
-then
+if [ -e /Applications/Postgres.app ]; then
   export PATH="/Applications/Postgres.app/Contents/MacOS/bin:$PATH"
 fi
 
-if $(/usr/libexec/java_home &>/dev/null)
-then
+if $(/usr/libexec/java_home &>/dev/null); then
   export JAVA_HOME=$(/usr/libexec/java_home)
 fi
