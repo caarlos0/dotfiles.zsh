59287ea7f99132826a92af973d46288f27c145bc
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-09-07 20:24:24 -0300

    fixed git completions

--- git/completion.zsh
+++ git/completion.zsh
@@ -2,10 +2,14 @@
 # bash `git-completion` script at $completion below (this is where Homebrew
 # tosses it, at least).
 #completion=/usr/local/etc/bash_completion.d/git-completion.bash
-#completion=$(brew --prefix)/share/zsh/site-functions/_git 
-completion=~/.homebrew/share/zsh/site-functions/_git
+#completion=$(brew --prefix)/share/zsh/site-functions/_git
 
-if test -f $completion
-then
+if [ "$(uname -s)" != "Darwin" ]; then
+  completion=/usr/local/etc/bash_completion.d/git-completion.bash
+else
+  completion='$(brew --prefix)/share/zsh/functions/_git'
+fi
+
+if test -f $completion; then
   source $completion
 fi
