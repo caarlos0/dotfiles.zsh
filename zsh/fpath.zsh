36dcc524403e801dbcd658fcc642267291dbbe59
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-09-30 21:20:03 -0300

    added zsh-users/zsh-completions

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,2 +1,5 @@
 #add each topic folder to fpath so that they can add functions and completion scripts
 for topic_folder ($ZSH/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+
+# add https://github.com/zsh-users/zsh-completions
+fpath=($ZSH/zsh/completions/src $fpath)
