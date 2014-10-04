2b4b054d45923abbb4086e3907fe2de637e9cb78
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-10-04 00:59:12 -0300

    Improved git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,6 +1,6 @@
 alias gl='git pull --prune'
 alias glg="git log --all --graph --decorate --oneline --abbrev-commit"
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glog=glg
 alias gp='git push origin HEAD'
 alias gpa='git push origin --all'
 alias gd='git diff'
@@ -12,10 +12,9 @@ alias ga='git add'
 alias gaa='git add -A'
 alias gcm='git commit -m'
 alias gcam='git commit -a -m'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gs='git status -sb'
+alias gcb='git-copy-branch-name'
 
 function gi() {
   curl -s "https://www.gitignore.io/api/${@}";
 }
-
