dbf569bcb7c366f9d0c80562fd6ca83ab1de3f55
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-10-04 00:59:27 -0300

    ssh alias in its right place

--- ssh/aliases.zsh
+++ ssh/aliases.zsh
@@ -6,3 +6,5 @@
 function send_key {
   cat ~/.ssh/id_rsa.pub | ssh ${1} "cat >> ~/.ssh/authorized_keys"
 }
+
+alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
