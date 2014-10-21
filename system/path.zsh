26d5d461a9d764d53d45448cbb967dc5743bb276
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-10-20 23:30:11 -0200

    PATHs

--- system/path.zsh
+++ system/path.zsh
@@ -1,10 +1,2 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
-if [ -e /Applications/Postgres.app ]; then
-  export PATH="/Applications/Postgres.app/Contents/MacOS/bin:$PATH"
-fi
-
-if $(/usr/libexec/java_home &>/dev/null); then
-  export JAVA_HOME=$(/usr/libexec/java_home)
-fi
