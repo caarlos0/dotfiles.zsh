5758c0223b8a2d852f846fbc10a26af451be83cc
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-10-25 16:45:10 -0200

    Brew aliases

--- homebrew/aliases.zsh
+++ homebrew/aliases.zsh
@@ -1,5 +1,4 @@
-if $(brew &>/dev/null)
-then
+if test $(which brew); then
   alias brewcleanup="brew cleanup && brew cask cleanup"
   alias brewupgrade="brew update && brew upgrade && brewcleanup"
 fi
--- homebrew/install.sh
+++ homebrew/install.sh
@@ -7,8 +7,7 @@
 [[ "$(uname -s)" != "Darwin" ]] && exit 0
 
 # Check for Homebrew
-if test ! $(which brew)
-then
+if test ! $(which brew); then
   echo "  Installing Homebrew for you."
   ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)" > /tmp/homebrew-install.log
 fi
