9e7a40b879560a0052ec8255f09e6ec6b5f45ad9
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-10-25 16:45:57 -0200

    fix brew url

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,7 +9,7 @@
 # Check for Homebrew
 if test ! $(which brew); then
   echo "  Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)" > /tmp/homebrew-install.log
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" > /tmp/homebrew-install.log
 fi
 
 brew update
