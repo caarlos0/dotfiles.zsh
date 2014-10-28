f282df93b191fcfbe70167fd94a450e8e12064db
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-10-28 21:54:44 -0200

    fixed when no rbenv

--- ruby/path.zsh
+++ ruby/path.zsh
@@ -1,2 +1,7 @@
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+if [ -d HOME/.rbenv ]; then
+  export PATH="$HOME/.rbenv/bin:$PATH"
+fi
+if $(rbenv &>/dev/null)
+then
+  eval "$(rbenv init -)"
+fi
