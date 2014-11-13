b4573e0db06b9588587afcdb06ad5ca7542f1193
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-11-13 00:14:03 -0200

    zsh force rehash

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,3 +1,6 @@
+# forces zsh to realize new commands
+zstyle ':completion:*' completer _oldlist _expand _force_rehash _complete
+
 # matches case insensitive for lowercase
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
