5dce7cbb599e2981f0e948dff66d9dff0bb4e8f7
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-11-23 09:33:05 -0200

    replacing 1password with lastpass

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -23,7 +23,7 @@ brew install brew-cask
 brew install grc coreutils the_silver_searcher htop-osx heroku-toolbelt \
   imagemagick wget unrar
 brew cask install iterm2 the-unarchiver disk-inventory-x appcleaner diffmerge \
-  onepassword
+  lastpass-universal
 
 # virtualization
 brew cask install virtualbox vagrant
