db266324a3993eae44a503065ce46859bd54d99b
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-12-10 01:23:10 -0200

    cleanup and improved bootstrap process
    - tabs vs spaces and shit
    - running homebrew/install.sh first
    - bumping all submodules and repo itself before running
    - minor fixes

--- android/install.sh
+++ android/install.sh
@@ -3,5 +3,5 @@ if [ "$(uname -s)" = "Darwin" ]; then
   brew cask install android-file-transfer
   # disable autoopen when connect.
   mv /opt/homebrew-cask/Caskroom/android-file-transfer/latest/Android\ File\ Transfer.app/Contents/Resources/Android\ File\ Transfer\ Agent.app{,_DISABLED}
-  mv ~/Library/Application\ Support/Google/Android\ File\ Transfer/Android\ File\ Transfer\ Agent.app{,_DISABLED}
+  mv $HOME/Library/Application\ Support/Google/Android\ File\ Transfer/Android\ File\ Transfer\ Agent.app{,_DISABLED}
 fi
