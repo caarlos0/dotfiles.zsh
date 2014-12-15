24ba8da1ca8b1475774c48686236ecb78fd85350
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-12-15 20:47:47 -0200

    better build.sh

--- build.sh
+++ build.sh
@@ -6,24 +6,37 @@ success() {
 fail() {
   printf "\r\033[2K  [\033[0;31mFAIL\033[0m] Linting %s...\n" "$1"
   exit 1
- }
+}
 
 check() {
   local script="$1"
-  shellcheck -e SC2001 "$script" || fail "$script"
+  shellcheck "$script" || fail "$script"
   success "$script"
 }
 
-check "./build.sh"
-check "./zsh/zshrc.symlink"
+check_shs() {
+  find .  -maxdepth 2 -type f -name "*.*sh" | while read script; do
+    check "$script"
+  done
+}
 
-find .  -maxdepth 2 -type f -name "*.*sh" | while read script; do
-  check "$script"
-done
+check_no_ext() {
+  find . -maxdepth 2 -type f ! -name "*.*" | grep -v ".git" | \
+    while read script; do
+    head=$(head -n1 "$script")
+    [[ "$head" = "#!/usr/bin/env ruby" ]] && continue
+    [[ "$head" =~ ^#compdef.* ]] && continue
+    check "$script"
+  done
+}
 
-find . -maxdepth 2 -type f ! -name "*.*" | egrep -vi ".git|*.md" | while read script; do
-  head=$(head -n1 "$script")
-  [[ "$head" = "#!/usr/bin/env ruby" ]] && continue
-  [[ "$head" =~ ^#compdef.* ]] &&  continue
-  check "$script"
-done
+main() {
+  check "./build.sh"
+  check "./zsh/zshrc.symlink"
+  check_shs
+  check_no_ext
+}
+
+[[ "$DEBUG" = "1" ]] && set -x
+
+main
