81153122e84f37fa8cd6b66d7dc59c4e592c4a9d
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-12-27 19:13:21 -0200

    more stuff

--- antigen/path.zsh
+++ antigen/path.zsh
@@ -2,6 +2,9 @@
 source "$ZSH/antigen/antigen/antigen.zsh"
 
 antigen bundle rupa/z
+antigen bundle Tarrasch/zsh-bd
+antigen bundle djui/alias-tips
+antigen bundle caarlos0/zsh-mkc
 antigen bundle sindresorhus/pure
 antigen bundle oknowton/zsh-dwim
 antigen bundle Tarrasch/zsh-colors
