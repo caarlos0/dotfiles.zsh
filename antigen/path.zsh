aa5c8ea9598ec87134e855ad5dba02728ccd73cb
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-12-28 19:23:55 -0200

    antigen bundles should be in zshrc

--- antigen/path.zsh
+++ antigen/path.zsh
@@ -1,16 +1,2 @@
 #!/bin/zsh
 source "$ZSH/antigen/antigen/antigen.zsh"
-
-antigen bundle rupa/z
-antigen bundle Tarrasch/zsh-bd
-antigen bundle djui/alias-tips
-antigen bundle caarlos0/zsh-mkc
-antigen bundle sindresorhus/pure
-antigen bundle oknowton/zsh-dwim
-antigen bundle Tarrasch/zsh-colors
-antigen bundle caarlos0/zsh-git-sync
-antigen bundle zsh-users/zsh-completions
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle zsh-users/zsh-history-substring-search
-
-antigen apply
