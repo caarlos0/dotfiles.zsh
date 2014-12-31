8a3024c13bc9edbb068052e11e7e1e005551bde7
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-12-31 17:34:40 -0200

    weirdness

--- ssh/aliases.zsh
+++ ssh/aliases.zsh
@@ -1,5 +1,2 @@
 #!/bin/zsh
-pubkey() {
-  more ~/.ssh/id_rsa.pub | pbcopy
-  echo '=> Public key copied to pasteboard.'
-}
+alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
