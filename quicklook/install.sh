dfda00187f52c8e833f98265992fd1a747e887d7
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-01-10 15:35:44 -0200

    Updated quicklook plugins

--- quicklook/install.sh
+++ quicklook/install.sh
@@ -2,4 +2,5 @@
 [[ "$(uname -s)" != "Darwin" ]] && exit 0
 # quick look plugins - https://github.com/sindresorhus/quick-look-plugins
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json \
-  qlprettypatch quicklook-csv betterzipql webp-quicklook suspicious-package
+  qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook \
+  suspicious-package
