cbb2afbef7d131b1b184c9ba2c62fbcf21c16e72
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-01-11 00:18:48 -0200

    disable spotify web helper

--- media/install.sh
+++ media/install.sh
@@ -7,3 +7,6 @@ brew cask install tvshows subtitle-master beamer vlc transmission spotify
 
 # disable iTunes fuckin helper
 sudo mv /Applications/iTunes.app/Contents/MacOS/iTunesHelper.app{,-disabled}
+
+# also this spotify web helper
+mv ~/Applications/Spotify.app/Contents/MacOS/SpotifyWebHelper{,-disabled}
