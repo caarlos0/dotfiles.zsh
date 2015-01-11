1ea286c6326a011054b08cb2a0fb2d9a282cd630
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-01-11 00:05:29 -0200

    disable itunes fucking helper

--- media/install.sh
+++ media/install.sh
@@ -4,3 +4,6 @@
 # watch and download stuff
 brew install youtube-dl
 brew cask install tvshows subtitle-master beamer vlc transmission spotify
+
+# disable iTunes fuckin helper
+sudo mv /Applications/iTunes.app/Contents/MacOS/iTunesHelper.app{,-disabled}
