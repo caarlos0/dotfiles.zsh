39255020da9c6f2f8183a8f6e8e4a9a97602c9e7
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-01-16 21:14:43 -0200

    fixed command-t and bump vimfiles

--- vim/install.sh
+++ vim/install.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
+#!/bin/zsh
+setup_command_t() {
+  cd "$ZSH"/vim/vimfiles/vim.symlink/bundle/command-t/ruby/command-t
+  ruby extconf.rb
+  make
+}
+setup_command_t
 vim +PluginInstall +qall
+
