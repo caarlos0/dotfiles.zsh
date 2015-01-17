4425193f2d862948b67046c53a82b0c457c6fef1
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-01-17 17:16:54 -0200

    added githubpulse

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -22,7 +22,8 @@ brew install brew-cask
 # usefull stuff
 brew install grc coreutils the_silver_searcher htop-osx heroku-toolbelt \
   imagemagick wget unrar ffmpeg gifsicle
-brew cask install iterm2 the-unarchiver disk-inventory-x appcleaner diffmerge
+brew cask install iterm2 the-unarchiver disk-inventory-x appcleaner diffmerge \
+  githubpulse
 
 # virtualization
 brew cask install virtualbox vagrant
