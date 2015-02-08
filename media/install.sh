f6bcf993d73bb692c93c183d6bdff20ac3d97d9d
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-02-08 11:17:50 -0200

    added ps3 media server

--- media/install.sh
+++ media/install.sh
@@ -3,7 +3,8 @@
 
 # watch and download stuff
 brew install youtube-dl
-brew cask install tvshows subtitle-master beamer vlc transmission spotify
+brew cask install tvshows subtitle-master beamer vlc transmission spotify \
+  ps3-media-server
 
 # disable iTunes fuckin helper
 sudo mv /Applications/iTunes.app/Contents/MacOS/iTunesHelper.app{,-disabled}
