23b9f0134647a733542590adc658436ba56285f1
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-02-26 00:34:06 -0300

    Disabled iTunes and Spotify helpers

--- media/install.sh
+++ media/install.sh
@@ -7,7 +7,7 @@ brew cask install tvshows subtitle-master beamer vlc transmission spotify \
   ps3-media-server
 
 # disable iTunes fuckin helper
-sudo mv /Applications/iTunes.app/Contents/MacOS/iTunesHelper.app{,-disabled}
+sudo mv /Applications/iTunes.app/Contents/MacOS/iTunesHelper.app{,-disabled} || true
 
 # also this spotify web helper
-mv ~/Applications/Spotify.app/Contents/MacOS/SpotifyWebHelper{,-disabled}
+mv ~/Applications/Spotify.app/Contents/MacOS/SpotifyWebHelper{,-disabled} || true
