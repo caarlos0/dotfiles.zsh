4236902e37a389cc80c4ba4e2da94244b854e264
Eduardo Rabelo <oieduardorabelo@gmail.com>
2015-03-21 21:50:58 +1300

    Updating Homebrew install path
    The previous URL returned this message:
    
    ```
    Whoops, the Homebrew installer has moved! Please instead run:
    
    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
    
    Also, please ask wherever you got this link from to update it to the above.
    Thanks!
    ```

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,7 +9,7 @@
 # Check for Homebrew
 if test ! "$(which brew)"; then
   echo "  Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 brew update
