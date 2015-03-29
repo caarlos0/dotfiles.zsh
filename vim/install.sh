415b3b32d257cfe684c7c745e3d4bb18e0962e22
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-03-29 00:55:41 -0300

    vim install still not working right

--- vim/install.sh
+++ vim/install.sh
@@ -5,5 +5,5 @@ setup_command_t() {
   make
 }
 [ "$(uname -s)" = "Darwin" ] && brew install vim
-vim +PluginInstall +qall >/tmp/vim-plugins.log 2>&1
-setup_command_t >/tmp/cmd-t.log 2>&1
+vim +PluginInstall +qall >/tmp/vim-plugins.log 2>&1 || true
+setup_command_t >/tmp/cmd-t.log 2>&1 || true
