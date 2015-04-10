4d55ca072ac8374fb2e0d32f39e3d3e4a757ccde
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-04-09 23:40:29 -0300

    fixing maven stuff

--- java/aliases.zsh
+++ java/aliases.zsh
@@ -1,8 +1,7 @@
 #!/bin/zsh
-# aliases
-
 if test "$(which terminal-notifier)"; then
   mvn() {
+    # shellcheck disable=SC2048,SC2086
     command mvn $*
     terminal-notifier -message "'mvn $*' done!"
   }
@@ -23,14 +22,3 @@ alias mvnnew='mvn archetype:generate -DarchetypeArtifactId=maven-archetype-quick
 killjboss() {
   pgrep -f jboss-modules | xargs kill -9
 }
-
-function listMavenCompletions {
-	reply=(
-		cli:execute cli:execute-phase archetype:generate generate-sources compile \
-    clean install test test-compile deploy package cobertura:cobertura \
-    jetty:run gwt:run gwt:debug -DskipTests -Dmaven.test.skip=true \
-    -DarchetypeCatalog=http://tapestry.formos.com/maven-snapshot-repository \
-    -Dtest=$(if [ -d ./src ] ; then find ./src -type f | grep -v svn | sed 's?.*/\([^/]*\)\..*?-Dtest=\1?'; fi));
-}
-
-compctl -K listMavenCompletions mvn
