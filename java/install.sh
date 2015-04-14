4837cf960143606870d351a39d1285e407b0e55c
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-04-13 21:35:51 -0300

    improved eclipse and java configs

--- java/install.sh
+++ java/install.sh
@@ -4,3 +4,13 @@
 # install some java stuff
 brew install maven
 brew cask install eclipse-jee
+
+BINARY="$(brew cask info eclipse-jee | grep opt | cut -f1 -d' ')/eclipse/Eclipse.app/Contents/MacOS/eclipse"
+
+if [ ! -z "$BINARY" ]; then
+  "$BINARY" \
+    -application org.eclipse.equinox.p2.director \
+    -noSplash \
+    -repository http://update.eclemma.org/ \
+    -installIUs com.mountainminds.eclemma.feature.feature.group
+fi
