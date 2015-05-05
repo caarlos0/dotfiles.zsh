fdb8bbc1517c3a42aa48cf7cb1ce0a406a9bf36a
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2015-05-05 16:30:40 -0300

    atom: language-puppet

--- atom.symlink/install.sh
+++ atom.symlink/install.sh
@@ -2,6 +2,6 @@
 [ "$(uname -s)" = "Darwin" ] && brew cask install atom
 apm install \
   editorconfig autocomplete-plus \
-  language-docker language-diff \
+  language-docker language-diff language-puppet \
   linter linter-jshint linter-ruby \
   atom-beautify color-picker || true
