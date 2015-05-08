ecfd59c8880c1568839708f6e11fbd14b393a0d7
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-05-07 23:05:46 -0300

    docker-compose

--- docker/install.sh
+++ docker/install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 if [ "$(uname -s)" = "Darwin" ]; then
+  curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
   brew cask install kitematic
 fi
