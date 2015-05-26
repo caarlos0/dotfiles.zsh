00a8d4e9d9e9719e900c7f84dac0a2fc29c26f26
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-05-25 22:37:31 -0300

    improvements

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,15 +1,14 @@
 #!/bin/zsh
-SITE_FUNCTIONS_PATH=/usr/local/share/zsh/site-functions
 PURE_PATH=~/.antigen/repos/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure.git
 
 antigen bundle sindresorhus/pure
 antigen apply
 
-ln -sf "$PURE_PATH/pure.zsh" "$SITE_FUNCTIONS_PATH/prompt_pure_setup"
-ln -sf "$PURE_PATH/async.zsh" "$SITE_FUNCTIONS_PATH/async"
+ln -sf "$PURE_PATH/pure.zsh" "$PURE_PATH/prompt_pure_setup"
 
-fpath=($SITE_FUNCTIONS_PATH $fpath[@])
 autoload -U promptinit && promptinit
+
 PURE_CMD_MAX_EXEC_TIME=1
 PURE_PROMPT_SYMBOL=λ
+
 prompt pure
