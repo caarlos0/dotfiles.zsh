80dba77aeada820e2c4cab948f31cca04f85be35
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2015-05-28 16:44:37 -0300

    fix prompt with new instructions

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,10 +1,7 @@
 #!/bin/zsh
-antigen bundle caarlos0/pure --branch=118
-antigen apply
-
-autoload -U promptinit && promptinit
-
 PURE_CMD_MAX_EXEC_TIME=1
 PURE_PROMPT_SYMBOL=λ
 
-prompt pure
+antigen bundle mafredri/zsh-async
+antigen bundle sindresorhus/pure
+antigen apply
