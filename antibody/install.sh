9f65a63025779db798425f152bb17ec91cf242f2
Carlos A Becker <caarlos0@gmail.com>
2015-06-08 08:24:28 -0300

    fixed url - added linux support

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-VERSION="0.0.3-Darwin"
+VERSION="0.0.3"
 mkdir -p ~/.dotfiles/antibody/antibody
 wget -O /tmp/antibody.tar.gz \
-  "https://github.com/caarlos0/antibody/releases/download/$VERSION/antibody-$VERSION.tar.gz"
+  "https://github.com/caarlos0/antibody/releases/download/$VERSION/antibody-$VERSION-$(uname -s).tar.gz"
 tar xvzf /tmp/antibody.tar.gz -C ~/.dotfiles/antibody/antibody
