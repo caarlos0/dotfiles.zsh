4e363d3a8258782f3be3fac2bb9d8171050ebd66
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2015-06-29 09:08:05 -0300

    support for non-standard zsh plugins

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 BASE_URL="https://github.com/caarlos0/antibody/releases/download"
-VERSION="v0.0.7"
+VERSION="v0.1.0"
 ARCH="386"
 OS="$(uname -s | tr "[:upper:]" "[:lower:]")"
 if [ "$(uname -m)" = "x86_64" ]; then
