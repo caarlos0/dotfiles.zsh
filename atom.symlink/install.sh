443060cd27c5aff954d6e1ed7772c0bbd87752a4
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-07-11 19:17:26 -0300

    improved golang support

--- atom.symlink/install.sh
+++ atom.symlink/install.sh
@@ -2,6 +2,13 @@
 [ "$(uname -s)" = "Darwin" ] && brew cask install atom
 apm install \
   editorconfig \
-  language-docker language-diff language-puppet \
-  linter linter-jshint linter-ruby \
-  atom-beautify color-picker go-plus go-rename || true
+  language-docker \
+  language-diff \
+  language-puppet \
+  linter \
+  linter-jshint \
+  linter-ruby \
+  atom-beautify \
+  color-picker \
+  go-plus \
+  go-rename || true
--- golang/install.sh
+++ golang/install.sh
@@ -2,6 +2,11 @@
 if [ "$(uname -s)" = "Darwin" ]; then
   brew install go hg
 fi
-go get -v golang.org/x/tools/cmd/gorename
-go get -v golang.org/x/tools/cmd/cover
-go get -v github.com/mitchellh/gox
+go get -u golang.org/x/tools/cmd/godoc
+go get -u golang.org/x/tools/cmd/gorename
+go get -u golang.org/x/tools/cmd/cover
+go get -u github.com/mitchellh/gox
+go get -u github.com/alecthomas/gometalinter
+
+"$GOPATH"/bin/gox -build-toolchain
+"$GOPATH"/bin/gometalinter --install --update
