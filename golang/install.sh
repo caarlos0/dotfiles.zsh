d156eb970e7fd97524b9fa15d6f0ba6ec8b4d92c
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-07-17 21:25:27 -0300

    go repl

--- golang/install.sh
+++ golang/install.sh
@@ -2,11 +2,18 @@
 if [ "$(uname -s)" = "Darwin" ]; then
   brew install go hg
 fi
+# docs
 go get -u golang.org/x/tools/cmd/godoc
+# rename funcs
 go get -u golang.org/x/tools/cmd/gorename
+# coverage
 go get -u golang.org/x/tools/cmd/cover
+# multi-arch compiler
 go get -u github.com/mitchellh/gox
+# linter
 go get -u github.com/alecthomas/gometalinter
+# repl
+go get -u github.com/motemen/gore
 
 "$GOPATH"/bin/gox -build-toolchain
 "$GOPATH"/bin/gometalinter --install --update
