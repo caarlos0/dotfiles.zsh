a4adedc00e8413b6303d1eb38515f1b914ac1e63
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-08-30 11:06:52 -0300

    New favorite font: Hack

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 if [ "$(uname -s)" = "Darwin" ]; then
-  brew cask install font-source-code-pro font-fira-code
+  brew cask install font-hack
 else
   FONTS_FOLDER="$HOME/.fonts"
   mkdir -p "$FONTS_FOLDER"
-  # Fira Code
-  if [ ! -f "$FONTS_FOLDER"/FiraCode-Regular.otf ]; then
-    wget -c https://github.com/tonsky/FiraCode/releases/download/0.3/FiraCode-Regular.otf \
-      -O "$FONTS_FOLDER"/FiraCode-Regular.otf
-  fi
-
-  # Source Code Pro
-  if [ ! -f "$FONTS_FOLDER"/SourceCodePro-Regular.ttf ]; then
-    wget -c https://github.com/adobe-fonts/source-code-pro/archive/1.017R.tar.gz \
-      -O /tmp/source-code-pro.tar.gz
-    mkdir -p /tmp/source-code-pro
-    tar -xzvf /tmp/source-code-pro.tar.gz -C /tmp/source-code-pro
-    mv /tmp/source-code-pro/*/OTF/* "$FONTS_FOLDER"/
-  fi
+  wget -O /tmp/hack.zip \
+   -c https://github.com/chrissimpkins/Hack/releases/download/v2.010/Hack-v2_010-ttf.zip
+  unzip /tmp/hack.zip -d "$FONTS_FOLDER"
 fi
