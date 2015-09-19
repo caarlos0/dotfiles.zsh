79441d677d57998e1dd5ef1eb4cc8fd4dfb19efd
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-09-19 13:30:41 -0300

    bump antibody

--- antibody/install.sh
+++ antibody/install.sh
@@ -5,7 +5,7 @@ if [ "$(uname -s)" = "Darwin" ]; then
   return 0
 fi
 BASE_URL="https://github.com/caarlos0/antibody/releases/download"
-VERSION="v0.3.3"
+VERSION="v0.3.5"
 ARCH="386"
 OS="$(uname -s | tr "[:upper:]" "[:lower:]")"
 if [ "$(uname -m)" = "x86_64" ]; then
