81e9a27a29dec7458c2d62e831944fe418c15fe2
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-09-27 04:02:34 -0300

    improved antibody install

--- antibody/install.sh
+++ antibody/install.sh
@@ -4,14 +4,7 @@ if [ "$(uname -s)" = "Darwin" ]; then
   brew install antibody
   return 0
 fi
-BASE_URL="https://github.com/caarlos0/antibody/releases/download"
-VERSION="v0.3.5"
-ARCH="386"
-OS="$(uname -s | tr "[:upper:]" "[:lower:]")"
-if [ "$(uname -m)" = "x86_64" ]; then
-  ARCH="amd64"
-fi
 mkdir -p ~/.dotfiles/antibody/antibody
 wget -O /tmp/antibody.tar.gz \
-  "${BASE_URL}/${VERSION}/antibody_${OS}_${ARCH}.tar.gz"
+  "http://antibody.elasticbeanstalk.com/latest/$(uname -s)/$(uname -m)"
 tar xvzf /tmp/antibody.tar.gz -C ~/.dotfiles/antibody/antibody
