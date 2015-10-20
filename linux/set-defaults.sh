06506b507c166b44bfa7250628e3cc89a1c130b5
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2015-10-20 15:53:32 -0200

    set terminator as default shell on linux

--- linux/set-defaults.sh
+++ linux/set-defaults.sh
@@ -6,3 +6,7 @@ fi
 # disable autorun
 gsettings set org.gnome.desktop.media-handling autorun-never true
 
+# set terminator as default terminal
+if test "$(which terminator)"; then
+  gsettings set org.gnome.desktop.default-applications.terminal exec 'terminator'
+fi
