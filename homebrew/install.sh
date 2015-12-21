661dca892cbea53aee37a21dbdfbdc56b3b6c994
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-12-21 12:00:14 -0200

    more cleanup and fixes

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -12,7 +12,7 @@ if test ! "$(which brew)"; then
   # Install the correct homebrew for each OS type
   if test "$(uname)" = "Darwin"; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"; then
+  else
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
   fi
 fi
