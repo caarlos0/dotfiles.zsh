572daf5857db047f4650b8e417fddd1d7bdb0100
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-01-07 22:24:22 -0200

    fixed antibody path and install on linuxbrew

--- antibody/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ "$(uname -s)" = "Darwin" ]; then
-  return 0
-fi
-mkdir -p ~/.dotfiles/antibody/antibody
-wget -O /tmp/antibody.tar.gz \
-  "http://antibody.elasticbeanstalk.com/latest/$(uname -s)/$(uname -m)"
-tar xvzf /tmp/antibody.tar.gz -C ~/.dotfiles/antibody/antibody
--- antibody/path.zsh
+++ antibody/path.zsh
@@ -1,6 +1,2 @@
 #!/bin/zsh
-if [ "$(uname -s)" = "Darwin" ]; then
-  source "$(brew --prefix)/share/antibody.zsh"
-else
-  source "$ZSH/antibody/antibody/antibody.zsh"
-fi
+source "$(brew --prefix)/share/antibody.zsh"
