ca11d3027c227977a606743ee59877f684f1e098
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-01-13 11:21:17 -0200

    typo

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,7 +3,7 @@ PURE_CMD_MAX_EXEC_TIME=1
 PURE_GIT_PULL=0
 antibody bundle sindresorhus/pure
 
-# this two guys **MUST** be load last.
+# these two guys **MUST** be load last.
 antibody bundle <<EOF
 zsh-users/zsh-syntax-highlighting
 zsh-users/zsh-history-substring-search
