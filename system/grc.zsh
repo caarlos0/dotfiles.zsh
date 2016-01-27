2816d6a38353c9c8819573e57a3bfdfdc343e80d
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-01-26 22:46:26 -0200

    multiple shellcheck fixes

--- system/grc.zsh
+++ system/grc.zsh
@@ -2,5 +2,6 @@
 # GRC colorizes nifty unix tools all over the place
 # shellcheck disable=2039
 if grc &>/dev/null && ! brew &>/dev/null; then
+  # shellcheck disable=SC1090
   . "$(brew --prefix)/etc/grc.bashrc"
 fi
