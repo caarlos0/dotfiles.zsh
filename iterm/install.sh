52e15a1bba70ea6c41907b25a99c29afb8c7d267
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-04-05 19:56:11 -0300

    fix #184

--- iterm/install.sh
+++ iterm/install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 [ "$(uname -s)" != "Darwin" ] && exit 0
+sed "s;/Users/carlos/.dotfiles;$ZSH;" \
+  "$ZSH"/iterm/com.googlecode.iterm2.plist.example > "$ZSH"/iterm/com.googlecode.iterm2.plist
 defaults write com.googlecode.iterm2 "PrefsCustomFolder" -string "$ZSH/iterm"
 defaults write com.googlecode.iterm2 "LoadPrefsFromCustomFolder" -bool true
