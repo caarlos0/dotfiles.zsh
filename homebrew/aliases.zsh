a0a3ccf460473cf0a731e6d86335dae1ae0e29b5
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-04-10 15:10:56 -0300

    fixed linux init

--- homebrew/aliases.zsh
+++ homebrew/aliases.zsh
@@ -1,19 +1,20 @@
 #!/bin/sh
-which brew >/dev/null 2>&1 || return
-brew() {
-  case "$1" in
-    cleanup)
-      command brew cleanup
-      command brew cask cleanup
-      command brew prune
-      ;;
-    bump)
-      command brew update
-      command brew upgrade --all
-      brew cleanup
-      ;;
-    *)
-      command brew "$@"
-      ;;
-  esac
-}
+if which brew >/dev/null 2>&1; then
+  brew() {
+    case "$1" in
+      cleanup)
+        command brew cleanup
+        command brew cask cleanup
+        command brew prune
+        ;;
+      bump)
+        command brew update
+        command brew upgrade --all
+        brew cleanup
+        ;;
+      *)
+        command brew "$@"
+        ;;
+    esac
+  }
+fi
