f800371c4b0806e897473dc180f12a64a624a78c
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-05-30 14:23:57 -0300

    shellcheck

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,7 +1,9 @@
 #!/bin/sh
 if which docker >/dev/null 2>&1; then
   dcleanup() {
+    # shellcheck disable=SC2046
     docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
+    # shellcheck disable=SC2046
     docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
   }
 
