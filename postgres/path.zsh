c1791eca7765f731b038834974a2f17d222affb5
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-06-22 11:19:51 -0300

    fix

--- postgres/path.zsh
+++ postgres/path.zsh
@@ -1,4 +1,4 @@
 #!/bin/sh
-if [ "$(uname -s)" = "Darwin" ] && [ -e "$PATH:/Applications/Postgres.app" ]; then
-  export PATH="$PATH:/Applications/Postgres.app/Contents/Versions/9.5/bin"
+if [ "$(uname -s)" = "Darwin" ] && [ -e "/Applications/Postgres.app" ]; then
+  export PATH="$PATH:/Applications/Postgres.app/Contents/Versions/9.5/bin/"
 fi
