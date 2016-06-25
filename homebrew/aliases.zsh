8d7cfce825816f9c47f4090f0a7029656252eb54
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-06-25 15:37:52 -0300

    shellcheck

--- homebrew/aliases.zsh
+++ homebrew/aliases.zsh
@@ -6,7 +6,7 @@ if which brew >/dev/null 2>&1; then
         command brew cleanup --force
         command brew cask cleanup --force
         command brew prune
-        rm -rf $(brew --cache)
+        rm -rf "$(brew --cache)"
         ;;
       bump)
         command brew update
