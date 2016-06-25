a671b669c52e40da1367f97dfaa72a3c97b64e77
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-06-25 16:36:42 -0300

    fixes returns

--- atom.symlink/install.sh
+++ atom.symlink/install.sh
@@ -1,37 +1,36 @@
 #!/bin/sh
-test "$(which apm)" || return 0
-apm upgrade --confirm false
+if test "$(which apm)"; then
+  apm upgrade --confirm false
 
-modules="
-atom-beautify
-atom-wrap-in-tag
-color-picker
-editorconfig
-file-icons
-go-plus
-go-rename
-language-diff
-language-docker
-language-puppet
-language-terraform
-linter
-linter-jshint
-linter-ruby
-native-ui
-one-dark-syntax
-sort-lines
-wakatime
-"
+  modules="
+    atom-beautify
+    atom-wrap-in-tag
+    color-picker
+    editorconfig
+    file-icons
+    go-plus
+    go-rename
+    language-diff
+    language-docker
+    language-puppet
+    language-terraform
+    linter
+    linter-jshint
+    linter-ruby
+    native-ui
+    one-dark-syntax
+    sort-lines
+    wakatime
+  "
+  for module in $modules; do
+    apm install "$module" || true
+  done
 
-for module in $modules; do
-  apm install "$module" || true
-done
-
-modules="
-metrics
-exception-reporting
-"
-
-for module in $modules; do
-  apm remove "$module" || true
-done
+  modules="
+    metrics
+    exception-reporting
+  "
+  for module in $modules; do
+    apm remove "$module" || true
+  done
+fi
