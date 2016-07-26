4c464681caf8e8b3c05704352c9c9351b6b6eb58
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-07-26 20:06:26 -0300

    moved conditional git configs to git/install.zsh

---
+++ git/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Don't ask ssh password all the time
+if [ "$(uname -s)" = "Darwin" ]; then
+  git config --global user.helper osxkeychain
+else
+  git config --global user.helper cache
+fi
+
+# better diffs
+if which diff-so-fancy > /dev/null 2>&1; then
+  git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+fi
