148e01c21d1f198bc2b0c52aee067daed3a99044
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-08-01 23:45:47 -0300

    antibody static loading
    even faster :rocket:

--- antibody/install.sh
+++ antibody/install.sh
@@ -7,3 +7,17 @@ if [ "$(uname -s)" = "Darwin" ] && which brew >/dev/null 2>&1; then
 else
   curl -sL https://git.io/vwMNi | sh -s
 fi
+
+command antibody bundle < "$ZSH/antibody/bundles.txt" |
+  xargs -I {} echo "source {}" > ~/.bundles.txt
+
+# bundles that should be loaded last, in this specific order
+last_bundles="
+  sindresorhus/pure
+  zsh-users/zsh-syntax-highlighting
+  zsh-users/zsh-history-substring-search
+"
+for bundle in $last_bundles; do
+  command antibody bundle "$bundle" |
+    xargs -I {} echo "source {}" >> ~/.bundles.txt
+done
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,9 +1,2 @@
 #!/bin/zsh
 PURE_CMD_MAX_EXEC_TIME=1
-antibody bundle sindresorhus/pure
-
-# these two guys **MUST** be loaded last.
-antibody bundle <<EOF
-  zsh-users/zsh-syntax-highlighting
-  zsh-users/zsh-history-substring-search
-EOF
