6f0ade32e5ae1ff9412d1f7e1463f39aa35f2b81
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-08-07 14:10:07 -0300

    using antibody new static-loading feature

--- antibody/install.sh
+++ antibody/install.sh
@@ -10,8 +10,6 @@ fi
 
 # this code could be simpler, but right now it's a hack for an antibody bug:
 #   https://github.com/getantibody/antibody/issues/119
-echo "" > ~/.bundles.txt
-for pack in main prompt last; do
-  command antibody bundle < "$ZSH/antibody/$pack.txt" |
-    xargs -I {} echo "source {}" >> ~/.bundles.txt
-done
+antibody bundle --static < "$ZSH/antibody/bundles.txt" > ~/.bundles.txt
+antibody bundle --static sindresorhus/pure >> ~/.bundles.txt
+antibody bundle --static < "$ZSH/antibody/last_bundles.txt" >> ~/.bundles.txt
