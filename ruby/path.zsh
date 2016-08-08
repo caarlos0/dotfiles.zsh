13a34251475bdc5f27dc9f619120540f0c4bc52e
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-08-08 20:23:34 -0300

    improved rbenv and pyenv

--- ruby/path.zsh
+++ ruby/path.zsh
@@ -1,7 +1,31 @@
 #!/bin/sh
 export PATH="$PATH:$HOME/.rbenv/bin"
 
+__rbenv_started=0
+
+__rbenv_init() {
+  test $__rbenv_started = 0 && {
+    eval "$(command rbenv init -)"
+    __rbenv_started=1
+  }
+}
+
 rbenv() {
-  eval "$(command rbenv init -)"
-  rbenv "$@"
+  __rbenv_init
+  command rbenv "$@"
+}
+
+ruby() {
+  __rbenv_init
+  command ruby "$@"
+}
+
+rake() {
+  __rbenv_init
+  command rake "$@"
+}
+
+bundle() {
+  __rbenv_init
+  command bundle "$@"
 }
