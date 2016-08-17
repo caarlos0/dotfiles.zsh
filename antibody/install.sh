be2fcc4af052c7859667a569a242d4484fe5994a
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-08-17 20:10:20 -0300

    Updating antibody on OSX on dot_update

--- antibody/install.sh
+++ antibody/install.sh
@@ -3,6 +3,9 @@ if [ "$(uname -s)" = "Darwin" ] && which brew >/dev/null 2>&1; then
   if [ ! "$(which antibody)" ]; then
     brew tap getantibody/homebrew-antibody
     brew install antibody
+  else
+    brew update
+    brew upgrade antibody
   fi
 else
   curl -sL https://git.io/vwMNi | sh -s
