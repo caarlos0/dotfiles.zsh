3228e9e200a2be4b99663ed1a28a2886ac4857ab
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-09-17 22:24:31 -0300

    option to keep terminator

--- terminator/install.sh
+++ terminator/install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 [ "$(uname -s)" = "Darwin" ] && exit 0
+[ "$KEEP_TERMINATOR" = "true" ] && exit 0
 mkdir -p ~/.config/terminator/
 ln -sf "$ZSH"/terminator/config ~/.config/terminator/config
