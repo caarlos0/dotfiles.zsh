4705e0afd9d9bbf654057449193d8b40375d67b3
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-10-30 11:56:01 -0200

    brew quiet

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if which brew >/dev/null 2>&1; then
-  brew untap getantibody/homebrew-antibody || true
-  brew tap getantibody/homebrew-antibody
+  brew untap -q getantibody/homebrew-antibody || true
+  brew tap -q getantibody/homebrew-antibody
   brew install antibody
 else
   curl -sL https://git.io/vwMNi | sh -s
