150f43549c952425bc3fd7fc7bcc7c8cb0512fea
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-01-06 18:26:43 -0200

    re-added st3

--- sublime-text-3/install.sh
+++ sublime-text-3/install.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-if [ "$(uname -s)" = "Darwin" ]; then
-  brew cask install sublime-text3
-  ST3_LOCAL="$HOME/Library/Application Support/Sublime Text 3"
-else
-  ST3_LOCAL="$HOME/.config/sublime-text-3"
-fi
+for d in \
+  "$HOME/Library/Application Support/Sublime Text 3" \
+  "$HOME/.config/sublime-text-3"
+do
+  test -d "$d" && {
+    ST3_LOCAL="$d"
+    break
+  }
+done
+
+# st3 is not installed
+test -n "$ST3_LOCAL" || exit 1
 
 # create needed directories
 mkdir -p "$ST3_LOCAL/Installed Packages"
@@ -18,12 +24,9 @@ PKG_CTRL_FILE="$ST3_LOCAL/Installed Packages/Package Control.sublime-package"
 
 # Link keymaps
 for platform in OSX Linux; do
-  ln -sf "$ZSH/sublime-text-3/Default.sublime-keymap" \
+  ln -sf "$DOTFILES/sublime-text-3/Default.sublime-keymap" \
     "$ST3_LOCAL/Packages/User/Default ($platform).sublime-keymap"
 done
 
 # Link all sublime-settings files
-ln -sf "$ZSH/sublime-text-3/"*.sublime-settings "$ST3_LOCAL/Packages/User/"
-
-unset ST3_LOCAL
-unset PKG_CTRL_FILE
+ln -sf "$DOTFILES"/sublime-text-3/*.sublime-settings "$ST3_LOCAL/Packages/User/"
