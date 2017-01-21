ee6a117321200f51d1039b9e28cba886e78bb186
Artem Medvedev <artdevjs@gmail.com>
2017-01-21 09:37:29 +0200

    removed terminator

--- terminator/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-[ "$(uname -s)" = "Darwin" ] && exit 0
-test -z "$KEEP_TERMINATOR" || exit 0
-mkdir -p ~/.config/terminator/
-ln -sf "$DOTFILES"/terminator/config ~/.config/terminator/config
