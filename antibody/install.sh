92c89393bcf1ec66e43c6f2d9adf7604cb797a0e
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-02-04 12:08:41 -0200

    improved brew install antibody

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 if which brew >/dev/null 2>&1; then
-  brew untap -q getantibody/homebrew-antibody || true
-  brew tap -q getantibody/homebrew-antibody
-  brew install antibody
+  brew install getantibody/tap/antibody
 else
   curl -sL https://git.io/antibody | sh -s
 fi
