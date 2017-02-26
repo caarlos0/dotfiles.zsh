6aa3986713e06b3eb5ba2fc7d99702f4619dc8a9
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-02-26 15:20:16 -0300

    speed up grc

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,6 +1,4 @@
 #!/bin/sh
 # GRC colorizes nifty unix tools all over the place
-if which grc >/dev/null 2>&1 && which brew >/dev/null 2>&1; then
-  # shellcheck disable=SC1090
-  . "$(brew --prefix)/etc/grc.bashrc"
-fi
+# shellcheck disable=SC1090,SC2086
+test -f /usr/local/etc/grc.bashrc && . $_
