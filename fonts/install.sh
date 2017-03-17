863e853390a52f8805c7ce5027c85bfea17dbd86
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-03-17 14:36:59 -0300

    anonymous pro font

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
-URL="https://github.com/chrissimpkins/Hack/releases/download/"
+URL="http://www.marksimonson.com/assets/content/fonts/AnonymousPro-1.002.zip"
 
 install() {
-  curl -L -s -o /tmp/hack.tgz "$URL/v2.020/Hack-v2_020-${1}.tar.gz"
-  tar xzvf /tmp/hack.tgz -C "$2"
+  curl -L -s -o /tmp/anon.tgz "$URL"
+  tar xzvf /tmp/anon.tgz -C /tmp
+  cp /tmp/AnonymousPro-1.002.001/*.ttf "$2"
 }
 
 if [ "$(uname -s)" = "Darwin" ]; then
   if which brew >/dev/null 2>&1; then
-     brew cask install font-hack
+     brew cask install font-anonymous-pro
   else
-    install "otf" ~/Library/Fonts
+    install ~/Library/Fonts
   fi
 else
   mkdir -p ~/.fonts
-  install "ttf" ~/.fonts
+  install ~/.fonts
 fi
