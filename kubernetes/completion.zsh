0d9889d4f96a0ce31a9c8d126a0368e94baffe43
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-04-27 10:35:32 -0300

    disabled kubectl completions because they are so fucking slow

--- kubernetes/completion.zsh
+++ kubernetes/completion.zsh
@@ -1,5 +1,6 @@
 #!/bin/sh
-if which kubectl > /dev/null 2>&1; then
-  # shellcheck disable=SC1090,SC2039
-  source <(kubectl completion zsh)
-fi
+# slow af
+# if which kubectl > /dev/null 2>&1; then
+#   # shellcheck disable=SC1090,SC2039
+#   source <(kubectl completion zsh)
+# fi
