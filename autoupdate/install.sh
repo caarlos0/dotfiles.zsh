773b60fc3c600f8e0397c2e0c8d6f73e334325df
Sean <norwood.sean@gmail.com>
2017-04-29 17:26:40 -0500

    Revert "remove updater"
    This reverts commit 2792396e5229d998127178f02fb721741a02a351.

---
+++ autoupdate/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# setups the auto-update
+(
+  crontab -l | grep -v "dot_update"
+  echo "0 */2 * * * $HOME/.dotfiles/bin/dot_update > ${TMPDIRR:-/tmp}/dot_update.log 2>&1"
+) | sort - | uniq - | crontab -
