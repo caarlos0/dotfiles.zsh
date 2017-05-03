f062b202868dde0f42b810a30777c8b4fc7b535f
Victor Bogo <B0go@users.noreply.github.com>
2017-05-03 17:18:33 -0300

    Update aliases.zsh

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -1,3 +1,8 @@
 #!/bin/sh
 alias k='kubectl'
-alias sk='kubectl -n kube-system'
+alias sk='k -n kube-system'
+alias kpods='k get pods'
+alias kdescb='k describe pods'
+alias klogs='k logs -f --tail 500'
+alias k-scale-zero='k scale deployment --replicas 0'
+alias k-scale-one='k scale deployment --replicas 1'
