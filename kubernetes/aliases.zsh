3fef3d1e9bd697163ed4b24b4c0216e196339247
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-05-09 10:14:05 -0300

    lazy-load kubectl completions

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -1,3 +1,10 @@
 #!/bin/sh
+
+kubectl() {
+  # shellcheck disable=SC1090,SC2039
+  source <(command kubectl completion zsh)
+  command kubectl "$@"
+}
+
 alias k='kubectl'
 alias sk='kubectl -n kube-system'
--- kubernetes/completion.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# slow af
-# if which kubectl > /dev/null 2>&1; then
-#   # shellcheck disable=SC1090,SC2039
-#   source <(kubectl completion zsh)
-# fi
