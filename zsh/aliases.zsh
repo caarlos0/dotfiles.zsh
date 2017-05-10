57e9e4aefafad1d7bb6bcdbbd164d44a572fda99
Sean Norwood <norwood.sean@gmail.com>
2017-05-10 14:56:13 -0500

    Added pacman orphan removal

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,5 +4,3 @@ __reload_dotfiles() {
 }
 alias reload!='__reload_dotfiles'
 alias orphans!='sudo pacman -Rns $(pacman -Qtdq)'
-alias minecraft!= './home/sean/minecraft/minecraft start'
-alias minecraft#= './home/sean/minecraft/minecraft stop'
