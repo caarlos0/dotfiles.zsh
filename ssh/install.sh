2e1d1c792e779061078ff700cd7b88fd2b74cf84
Sean Norwood <norwood.sean@gmail.com>
2017-05-20 15:00:40 -0500

    Remove ssh config

--- ssh/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-test -L ~/.ssh/config || {
-  mv ~/.ssh/config ~/.ssh/config.local
-  ln -s "$DOTFILES"/ssh/config ~/.ssh/config
-}
-test -f ~/.ssh/config.local || touch ~/.ssh/config.local
