6cfc3edb6aa9047b6288f3b99a259f6627a2d5ff
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-06-22 11:08:13 -0300

    vscode as mergetool

--- git/install.sh
+++ git/install.sh
@@ -11,3 +11,9 @@ fi
 if which diff-so-fancy >/dev/null 2>&1; then
 	git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 fi
+
+# use vscode as mergetool
+if which code >/dev/null 2>&1; then
+	git config --global merge.tool vscode
+	git config --global mergetool.vscode.cmd "code --wait $MERGED"
+fi
