4deb8018253c06c7773744c2f6a694d941a05b5c
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-07-19 09:09:47 -0300

    this is actually annoying me now

--- java/aliases.zsh
+++ java/aliases.zsh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-mvn() {
-	# shellcheck disable=SC2068
-	command mvn $@
-	message="'mvn $*' done!"
-	which terminal-notifier >/dev/null && terminal-notifier -message "$message"
-	which notify-send >/dev/null && notify-send "$message"
-}
-
 alias mvnci='mvn clean install'
 alias mvnc='mvn clean'
 alias mvnt='mvn test'
