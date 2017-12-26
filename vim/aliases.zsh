da258455e7ffff8234f8d9e9bc98c73366c5fc47
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-12-25 23:47:29 -0200

    vim: new configs

---
+++ vim/aliases.zsh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# neovim
+if which nvim >/dev/null 2>&2; then
+	alias vim='nvim'
+fi
--- vim/install.sh
+++ vim/install.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-vim +PluginInstall! +qall >/tmp/vim-plugins.log 2>&1 || true
+curl -fLo "$DOTFILES/vim/autoload/plug.vim" --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall
+
+if which neovim >/dev/null 2>&1; then
+	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+	mkdir -p ~/.config/nvim/
+	ln -sf "$DOTFILES/vim/vimrc.symlink" ~/.config/nvim/init.vim
+	nvim +PlugInstall
+fi
