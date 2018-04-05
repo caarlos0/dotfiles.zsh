a77188202660bedcc434c3957de771646224bd6c
Carlos Alexandro Becker <caarlos0@gmail.com>
2018-04-05 16:52:19 -0300

    feat: more k8s aliases

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -14,7 +14,9 @@ kx() {
 alias k='kubectl'
 alias sk='kubectl -n kube-system'
 alias ke='EDITOR=vim kubectl edit'
+alias klbaddr="kubectl get svc -ojsonpath='{.status.loadBalancer.ingress[0].hostname}'"
 
 alias kdebug='kubectl run -i -t debug --rm --image=caarlos0/debug --restart=Never'
+alias knrunning='kubectl get pods --field-selector=status.phase!=Running'
 alias kfails='kubectl get po -owide --all-namespaces | grep "0/" | tee /dev/tty | wc -l'
 alias kimg="kubectl get deployment --output=jsonpath='{.spec.template.spec.containers[*].image}'"
