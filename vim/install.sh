47520091f2bda62576231be848ad6cb2870ebba8
Carlos Alexandro Becker <caarlos0@gmail.com>
2018-04-19 22:13:28 -0300

    vim: fix init

--- vim/install.sh
+++ vim/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl -fLo "$DOTFILES/vim/autoload/plug.vim" --create-dirs \
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall
 vim +PlugUpdate
