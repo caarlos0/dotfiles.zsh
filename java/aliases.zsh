801e7a2653f2d8c21d2761c3d641ef78a3ceb58f
Carlos Alexandro Becker <caarlos0@gmail.com>
2018-05-02 23:30:39 -0700

    clean: remove some unused java stuff

--- java/aliases.zsh
+++ java/aliases.zsh
@@ -1,11 +1,2 @@
 #!/bin/sh
-
 alias mvnci='mvn clean install'
-alias mvnc='mvn clean'
-alias mvnt='mvn test'
-alias mvnag='mvn archetype:generate'
-alias mvnnew='mvn archetype:generate -DarchetypeArtifactId=maven-archetype-quickstart'
-
-killjboss() {
-	pkill -9 -f jboss-modules
-}
