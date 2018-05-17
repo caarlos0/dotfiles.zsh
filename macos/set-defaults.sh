a6028bdc9636cea44e32cb47aabb039fda3d8b50
Carlos Alexandro Becker <caarlos0@gmail.com>
2018-05-16 22:06:30 -0300

    clean: remove twitter settings
    twitter.app was abandoned by twitter inc

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -303,31 +303,6 @@ disable_agent "$HOME/Library/Application Support/Google/Android File Transfer/An
 #############################
 
 echo ""
-echo "› Twitter.app:"
-echo "  › Disable smart quotes as it’s annoying for code tweets"
-defaults write com.twitter.twitter-mac AutomaticQuoteSubstitutionEnabled -bool false
-
-echo "  › Show the app window when clicking the menu bar icon"
-defaults write com.twitter.twitter-mac MenuItemBehavior -int 1
-
-echo "  › Enable the hidden ‘Develop’ menu"
-defaults write com.twitter.twitter-mac ShowDevelopMenu -bool true
-
-echo "  › Open links in the background"
-defaults write com.twitter.twitter-mac openLinksInBackground -bool true
-
-echo "  › Allow closing the new tweet window by pressing Esc"
-defaults write com.twitter.twitter-mac ESCClosesComposeWindow -bool true
-
-echo "  › Show full names rather than Twitter handles"
-defaults write com.twitter.twitter-mac ShowFullNames -bool true
-
-echo "  › Hide the app in the background if it’s not the front-most window"
-defaults write com.twitter.twitter-mac HideInBackground -bool true
-
-#############################
-
-echo ""
 echo "› Kill related apps"
 for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
 	"Dock" "Finder" "Mail" "Messages" "Safari" "SystemUIServer" \
