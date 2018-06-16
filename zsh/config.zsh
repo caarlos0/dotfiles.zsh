d62f20a26ac83a2e323fc75ef8ed2c109ffad30f
Carlos Alexandro Becker <caarlos0@gmail.com>
2018-06-16 20:00:45 -0300

    fix: zsh: option+left/right to go to beginning/end of line

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -51,7 +51,7 @@ bindkey "$terminfo[cuu1]" up-line-or-beginning-search
 bindkey "$terminfo[cud1]" down-line-or-beginning-search
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
+bindkey '^[[H' beginning-of-line
+bindkey '^[[F' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^?' backward-delete-char
