f304f0195f1d848545dcfda40721dc53cce8dbab
Carlos Alexandro Becker <caarlos0@gmail.com>
2018-06-17 12:49:17 -0300

    fix: zsh: ctrl+r

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -45,17 +45,36 @@ setopt RM_STAR_SILENT
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 
+# fuzzy find: start to type
 bindkey "$terminfo[kcuu1]" up-line-or-beginning-search
 bindkey "$terminfo[kcud1]" down-line-or-beginning-search
 bindkey "$terminfo[cuu1]" up-line-or-beginning-search
 bindkey "$terminfo[cud1]" down-line-or-beginning-search
+
+# backward and forward word with option+left/right
 bindkey '^[^[[D' backward-word
 bindkey '^[b' backward-word
 bindkey '^[^[[C' forward-word
 bindkey '^[f' forward-word
+
+# to to the beggining/end of line with fn+left/right or home/end
 bindkey "${terminfo[khome]}" beginning-of-line
 bindkey '^[[H' beginning-of-line
 bindkey "${terminfo[kend]}"  end-of-line
 bindkey '^[[F' end-of-line
+
+# delete char with backspaces and delete
 bindkey '^[[3~' delete-char
 bindkey '^?' backward-delete-char
+
+# delete word with ctrl+backspace
+bindkey '^[[3;5~' backward-delete-word
+bindkey '^[[3~' backward-delete-word
+
+# search history with fzf if installed, default otherwise
+if test -d /usr/local/opt/fzf/shell; then
+	# shellcheck disable=SC1091
+	. /usr/local/opt/fzf/shell/key-bindings.zsh
+else
+	bindkey '^R' history-incremental-search-backward
+fi
