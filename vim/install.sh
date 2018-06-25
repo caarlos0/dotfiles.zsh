a7e9aafe4dff36ebdb9b23548ff75b6c12342fbc
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2018-06-25 10:07:05 -0300

    chore: format

--- vim/install.sh
+++ vim/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall < /dev/tty
-vim +PlugUpdate < /dev/tty
+vim +PlugInstall </dev/tty
+vim +PlugUpdate </dev/tty
 
 if which neovim >/dev/null 2>&1; then
 	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
@@ -10,6 +10,6 @@ if which neovim >/dev/null 2>&1; then
 
 	mkdir -p ~/.config/nvim/
 	ln -sf "$DOTFILES/vim/vimrc.symlink" ~/.config/nvim/init.vim
-	nvim +PlugInstall < /dev/tty
-	nvim +PlugUpdate < /dev/tty
+	nvim +PlugInstall </dev/tty
+	nvim +PlugUpdate </dev/tty
 fi
