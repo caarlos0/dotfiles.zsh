57cc5527627e0a04e0edc48e57e22bf478c9407d
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2018-12-18 10:34:51 -0200

    fix: variable
    closes 349

--- autoupdate/install.sh
+++ autoupdate/install.sh
@@ -2,5 +2,5 @@
 # setups the auto-update
 (
 	crontab -l | grep -v "dot_update"
-	echo "0 */2 * * * $HOME/.dotfiles/bin/dot_update > ${TMPDIRR:-/tmp}/dot_update.log 2>&1"
+	echo "0 */2 * * * $HOME/.dotfiles/bin/dot_update > ${TMPDIR:-/tmp}/dot_update.log 2>&1"
 ) | crontab -
