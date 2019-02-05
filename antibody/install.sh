15f2abe0ac3d151927b9aea183686276f1c2b55c
Carlos Alexandro Becker <caarlos0@gmail.com>
2019-02-05 14:38:38 -0200

    fix(ci): brew install antibody

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 if command -v brew >/dev/null 2>&1; then
-	brew install getantibody/tap/antibody || brew upgrade antibody
+	brew tap | grep -q 'getantibody/tap' || brew tap getantibody/tap
+	brew install antibody
 else
 	curl -sL https://git.io/antibody | sh -s
 fi
