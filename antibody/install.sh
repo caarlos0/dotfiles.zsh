5ec525a490f0dd3658c96e50952e75311654f78f
Carlos Alexandro Becker <caarlos0@gmail.com>
2019-10-10 00:22:52 -0300

    fix: antibody install
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- antibody/install.sh
+++ antibody/install.sh
@@ -3,7 +3,7 @@ if command -v brew >/dev/null 2>&1; then
 	brew tap | grep -q 'getantibody/tap' || brew tap getantibody/tap
 	brew install antibody
 else
-	curl -sL https://git.io/antibody | sh -s
+	curl -sL https://git.io/antibody | sudo sh -s -- -b /usr/local/bin
 fi
 antibody bundle <"$DOTFILES/antibody/bundles.txt" >~/.zsh_plugins.sh
 antibody update
