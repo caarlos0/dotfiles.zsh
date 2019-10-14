681eb25bf439ee6b069c1c0bc8bcf867e5741889
Carlos Alexandro Becker <caarlos0@gmail.com>
2019-10-14 15:08:26 -0300

    feat: hammerspoon config
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

---
+++ hammerspoon/config.lua
@@ -0,0 +1,62 @@
+-- hyper key
+hyper = { 'alt', 'ctrl', 'cmd' }
+
+-- disable anymations
+hs.window.animationDuration = 0
+
+-- middle left
+hs.hotkey.bind(hyper, "Left", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- middle right
+hs.hotkey.bind(hyper, "Right", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- centralize at 80% screen size
+hs.hotkey.bind(hyper, 'C', function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local max = win:screen():frame()
+
+  f.x = max.x * 0.8
+  f.y = max.y * 0.8
+  f.w = max.w * 0.8
+  f.h = max.h * 0.8
+
+  win:setFrame(f)
+  win:centerOnScreen()
+end)
+
+-- maximize
+hs.hotkey.bind(hyper, 'M', function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local max = win:screen():frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h
+
+  win:setFrame(f)
+end)
---
+++ hammerspoon/install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mkdir -p "$HOME/.hammerspoon"
+ln -sf "$DOTFILES/hammerspoon/config.lua" "$HOME/.hammerspoon/init.lua"
