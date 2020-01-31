2af01db1bae550279662e6cc955ab6e32294cb07
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-01-31 12:02:53 -0300

    fix: croot -> cdr
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -12,7 +12,7 @@ alias grep="grep --color=auto"
 alias duf="du -sh * | sort -hr"
 alias less="less -r"
 
-alias croot='cd "$(git rev-parse --show-toplevel)"'
+alias cdr='cd "$(git rev-parse --show-toplevel)"'
 
 # quick hack to make watch work with aliases
 alias watch='watch '
