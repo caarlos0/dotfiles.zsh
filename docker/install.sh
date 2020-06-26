b17317b5337665dac6263e9b193f48f71876fe15
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-06-26 10:17:56 -0300

    fix: completions
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

---
+++ docker/install.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+wget -O ~/.config/fish/conf.d/docker_completions.fish https://raw.githubusercontent.com/docker/cli/master/contrib/completion/fish/docker.fish
---
+++ kubernetes/install.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+wget -O ~/.config/fish/conf.d/kubectl_completions.fish https://raw.githubusercontent.com/evanlucas/fish-kubectl-completions/master/completions/kubectl.fish
